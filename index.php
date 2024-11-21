803899b87fd8743b0146999fc4169769ac6fde35
YmarkVince <ymarkvincecabatbat@gmail.com>
2024-11-22 06:22:19 +0800

    Add function to login user

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+
+include('functions.php'); 
+
+
+$error_message = '';
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    
+    if (loginUser($email, $password)) {
+      
+        header("Location: ./admin/dashboard.php");
+        exit();
+    } else {
+       
+        $error_message = 'Invalid email or password.';
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,23 +29,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title></title>
+    <title>Login</title>
 </head>
 
 <body class="bg-secondary-subtle">
     <div class="d-flex align-items-center justify-content-center vh-100">
         <div class="col-3">
-            <!-- Server-Side Validation Messages should be placed here -->
+           
+            <?php if (!empty($error_message)): ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo htmlspecialchars($error_message); ?>
+                </div>
+            <?php endif; ?>
             <div class="card">
                 <div class="card-body">
                     <h1 class="h3 mb-4 fw-normal">Login</h1>
-                    <form method="post" action="">
+                    <form method="POST" action="">
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="email" name="email" placeholder="user1@example.com">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="user1@example.com" required>
                             <label for="email">Email address</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
                             <label for="password">Password</label>
                         </div>
                         <div class="form-floating mb-3">
